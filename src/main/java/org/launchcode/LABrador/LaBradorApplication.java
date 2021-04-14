4913eb1e64cd546560080ba125bf366729a90025
booma1191 <booma1191.m@gmail.com>
2021-04-14 14:37:40 -0500

    Initial Commit; Displays Hello World!

---
+++ src/main/java/org/launchcode/LABrador/LaBradorApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.LABrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaBradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaBradorApplication.class, args);
+	}
+
+}
