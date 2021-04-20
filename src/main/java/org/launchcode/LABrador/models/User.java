77f8775e5ebbf8f260e89ebe45128ef27185dd0a
unknown <hconway@luc.edu>
2021-04-19 19:41:34 -0500

    created user model

---
+++ src/main/java/org/launchcode/LABrador/models/User.java
@@ -0,0 +1,74 @@
+package org.launchcode.LABrador.models;
+
+public class User {
+
+    private int id;
+    private String username;
+    private String password;
+    private String lab;
+    private String email;
+    private String firstName;
+    private String lastName;
+
+    public User() {}
+
+    public User(int id, String username, String password, String email, String firstName, String lastName) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLab() {
+        return lab;
+    }
+
+    public void setLab(String lab) {
+        this.lab = lab;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
