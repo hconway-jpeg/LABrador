ef6077cf814c28dc65b186ea5897b1cb66e5a4a6
jaylyn dunlap <j.dunlap@aol.com>
2021-04-25 19:09:58 -0500

    login attempt

--- src/main/java/org/launchcode/LABrador/controllers/logincontroller.java
+++ src/main/java/org/launchcode/LABrador/controllers/logincontroller.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class logincontroller{
     @Autowired
     userRepository UserRepository;
-    @PostMapping("/users/register")
+    @PostMapping("/register")
     public Status registerUser(@RequestBody User newUser) {
         List<User> users = UserRepository.findAll();
         System.out.println("New user: " + newUser.toString());
@@ -28,8 +28,9 @@ public class logincontroller{
         }
         UserRepository.save(newUser);
         return Status.SUCCESS;
+        //redirect to login
     }
-    @PostMapping("/users/login")
+    @PostMapping("/login")
     public Status loginUser(@RequestBody User user) {
         List<User> users = UserRepository.findAll();
         for (User other : users) {
@@ -37,26 +38,10 @@ public class logincontroller{
                 user.setLoggedIn(true);
                 UserRepository.save(user);
                 return Status.SUCCESS;
+                //redirect
             }
         }
         return Status.FAILURE;
-    }
-    @PostMapping("/users/logout")
-    public Status logUserOut(@RequestBody User user) {
-        List<User> users = UserRepository.findAll();
-        for (User other : users) {
-            if (other.equals(user)) {
-                user.setLoggedIn(false);
-                UserRepository.save(user);
-                return Status.SUCCESS;
-            }
-        }
-        return Status.FAILURE;
-    }
-    @DeleteMapping("/users/all")
-    public Status deleteUsers() {
-        UserRepository.deleteAll();
-        return Status.SUCCESS;
     }
 }
 
