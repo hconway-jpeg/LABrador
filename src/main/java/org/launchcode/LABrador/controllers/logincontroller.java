32b173b6fa57cd2e646cd414d64ee214e824029b
jaylyn dunlap <j.dunlap@aol.com>
2021-04-25 19:25:09 -0500

    login attemp

--- src/main/java/org/launchcode/LABrador/controllers/logincontroller.java
+++ src/main/java/org/launchcode/LABrador/controllers/logincontroller.java
@@ -4,7 +4,6 @@ import org.launchcode.LABrador.data.Status;
 import org.launchcode.LABrador.data.userRepository;
 import org.launchcode.LABrador.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
