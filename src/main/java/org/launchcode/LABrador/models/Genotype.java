4737a48ac08e6e2847cf92aac3fb61cc14181aae
unknown <hconway@luc.edu>
2021-05-06 16:54:52 -0500

    working on adding drop-down menu for genotypes

---
+++ src/main/java/org/launchcode/LABrador/models/Genotype.java
@@ -0,0 +1,34 @@
+package org.launchcode.LABrador.models;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Genotype extends AbstractEntity {
+
+    @NotBlank
+    private String genotype;
+
+    @OneToMany(mappedBy = "genotype")
+    private final List<Animal> animals = new ArrayList<>();
+
+    public Genotype () {}
+
+    public Genotype(@NotBlank String genotype) {
+        this.genotype = genotype;
+    }
+
+    public String getGenotype() {
+        return genotype;
+    }
+    public void setGenotype(String genotype) {
+        this.genotype = genotype;
+    }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
+}
