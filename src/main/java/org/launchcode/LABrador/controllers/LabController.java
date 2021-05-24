c94408c593a7579d05f7421d879666cb4c077ca3
unknown <hconway@luc.edu>
2021-05-24 13:13:53 -0500

    created 'add lab' form, but only accessible while logged in. needs to be accessible prior to login

--- src/main/java/org/launchcode/LABrador/controllers/LabController.java
+++ src/main/java/org/launchcode/LABrador/controllers/LabController.java
@@ -1,7 +1,33 @@
 package org.launchcode.LABrador.controllers;
 
+import org.launchcode.LABrador.data.LabRepository;
+import org.launchcode.LABrador.data.UserRepository;
+import org.launchcode.LABrador.models.Lab;
+import org.launchcode.LABrador.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
+@RequestMapping("lab")
 public class LabController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private LabRepository labRepository;
+
+    @GetMapping
+    public String displayAddLabForm(Model model) {
+        model.addAttribute("title", "Add Lab");
+        model.addAttribute(new Lab());
+        return "lab/add";
+    }
+
 }
--- src/main/java/org/launchcode/LABrador/models/Lab.java
+++ src/main/java/org/launchcode/LABrador/models/Lab.java
@@ -1,7 +1,6 @@
 package org.launchcode.LABrador.models;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import java.util.*;
 
